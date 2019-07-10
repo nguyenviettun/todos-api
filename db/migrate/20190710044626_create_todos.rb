aa81ca7166e3ac7d2e9ba78e23887756d90021e8
iAmTun <tuan.nguyen@employmenthero.com>
2019-07-10 12:03:50 +0700

    Recreate models Todo and Item
    To fix foreign key constraint failed

--- db/migrate/20190710044626_create_todos.rb
+++ db/migrate/20190710044626_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :title
-      t.string :create_by
+      t.string :created_by
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_034905) do
+ActiveRecord::Schema.define(version: 2019_07_10_045449) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_10_034905) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
-    t.string "create_by"
+    t.string "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
